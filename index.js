1fd99ca91260c7bf94ffa9c61273f80ae9616f56
Will Simmons <willsimmons@gmail.com>
2018-07-05 22:12:16 -0700

    open API (swagger)
    - swagger-hapi add on and dependencies added and configured
    - sample documentation

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const {DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, SERVER_PORT, SERVER_HOST
 //hapi setup
 const hapi = require('hapi');
 const routes = require('./routes').routeTable;
+const inert = require('inert');
+const HapiSwagger = require('hapi-swagger');
+const Package = require('./package');
+const Vision = require('vision');
 const server = hapi.server({
   port: SERVER_PORT,
   host: SERVER_HOST,
@@ -21,41 +25,47 @@ mongoose.connection.once('open',() => {
   console.log('connected to MongoDB named testAPI');
 });
 
-//launch
 const init = async () => {
-  //graphql
+  //hapi plugins
   try{
-    await server.register({
-      plugin: graphqlHapi,
-      options: {
-        path: '/graphql',
-        graphqlOptions:{
-          schema
+    await server.register([
+      {
+        plugin: graphqlHapi,
+        options: {
+          path: '/graphql',
+          graphqlOptions:{
+            schema
+          },
+          route: {
+            cors: true,
+          }
         },
-        route: {
-          cors: true,
+      }, {
+        plugin: graphiqlHapi,
+        options: {
+          path: '/graphiql',
+          graphiqlOptions: {
+            endpointURL: '/graphql'
+          },
+          route: {
+            cors: true,
+          }
         }
       },
-    });
-  } catch(e) {
-    console.error('fix graphql');
-  }
-  //graphiql ide for graphQL, only for dev branch
-  try{
-    await server.register({
-      plugin: graphiqlHapi,
-      options: {
-        path: '/graphiql',
-        graphiqlOptions: {
-          endpointURL: '/graphql'
-        },
-        route: {
-          cors: true,
+      inert,
+      Vision,
+      {
+        plugin: HapiSwagger,
+        options: {
+          info: {
+            title: 'TestAPI API documentation',
+            version: Package.version
+          },
         }
       }
-    })
+    ]);
   } catch (e) {
-    console.error('graphiql is not configured')
+    console.error(e, 'plugin(s) not configured properly');
   }
   try {
     await server.route(routes);
@@ -65,7 +75,7 @@ const init = async () => {
   try {
     await server.start();
   } catch (e) {
-    console.error('cannot start');
+    console.error(e, 'cannot start');
   }
   console.log(`Server running at ${server.info.uri}`);
 };
--- routes.js
+++ routes.js
@@ -4,6 +4,10 @@ const routeTable = [
   {
     method: 'GET',
     path: '/',
+    config: {
+      description: 'Root Route',
+      tags: [ 'api', 'root', 'v1'],
+    },
     handler:  (request, reply) => {
       return '<h1>Hello World </h1>';
     },
@@ -11,6 +15,10 @@ const routeTable = [
   {
     method: 'GET',
     path: '/api/v1/items',
+    config: {
+      description: 'Get all of the Items',
+      tags: [ 'api', 'Items', 'v1'],
+    },
     handler: (request,reply) => {
       return Item.find();
     }
@@ -18,6 +26,10 @@ const routeTable = [
   {
     method: 'POST',
     path: '/api/v1/items',
+    config: {
+      description: 'Create a new Item',
+      tags: [ 'api', 'Items', 'v1'],
+    },
     handler: (request, reply) => {
       const {name, colors } = request.payload;
       const item = new Item({
